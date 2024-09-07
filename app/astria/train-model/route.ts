5b7a0562f8bf6382666e33bf1c0aa8bffbaee749
utopiaexpress123 <basepaxton@gmail.com>
2024-09-07 23:21:06 +0200

    tt

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -142,6 +142,7 @@ export async function POST(request: Request) {
             text: `portrait of ohwx ${type} wearing a business suit, professional photo, white background, Amazing Details, Best Quality, Masterpiece, dramatic lighting highly detailed, analog photo, overglaze, 80mm Sigma f/1.4 or any ZEISS lens`,
             negative_prompt:`ugly, old, unrealistic`,
             super_resolution: true,
+            inpaint_faces : true,
             callback: promptWebhookWithParams,
             num_images: 4,
           },
