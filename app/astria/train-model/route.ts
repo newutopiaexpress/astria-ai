d2bf68f53ba868935c2bdc4dd6630f290ff0cc15
utopiaexpress123 <basepaxton@gmail.com>
2024-09-07 20:40:20 +0200

    d

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -131,7 +131,7 @@ export async function POST(request: Request) {
         title: name,
         // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
         // https://www.astria.ai/gallery/tunes/690204/prompts 1034743
-        base_tune_id: 1504944,
+        base_tune_id: 690204,
         name: type,
         branch: astriaTestModeIsOn ? "fast" : "sd15",
         token: "ohwx",
