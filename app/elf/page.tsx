63b0687e27525d10b598f92b37d46a46f61d2e83
utopiaexpress123 <basepaxton@gmail.com>
2024-12-06 20:18:23 +0100

    kk

--- app/elf/page.tsx
+++ app/elf/page.tsx
@@ -11,7 +11,7 @@ export default function HelloWorldPage() {
     <div className="w-full ">
 	    <div className="w-full mx-auto z-30 bg-transparent">
           
-          <div className="text-center pt-24 md:pt-32 mb-32 md:mb-0">
+          <div className="text-center pt-40 md:pt-32 mb-32 md:mb-0">
             <InView
                 variants={{
                   hidden: { opacity: 0, y: 10, filter: 'blur(4px)' },
@@ -21,7 +21,8 @@ export default function HelloWorldPage() {
                 transition={{ duration: 0.4, ease: 'easeIn', delay: 3 }}
               >
               <p className="text-sm text-center mb-4 md:mb-0 md:mr-5">
-                        <span className="font-bold mr-2">Christmas Promo Gift!</span><span className="font-thin italic">🎄 Spotlight Series for free! 24pcs stunning photo</span> 
+                <span className="font-bold mr-2">Christmas Promo Gift!</span>
+                <span className="font-thin italic">🎄 Spotlight Photo Series for free!</span> 
               </p>
             </InView>
             <h1 className="text-center py-4 text-stone-800 font-thin tracking-tight drop-shadow-sm [text-wrap:balance] text-5xl leading-[2.5rem] md:text-6xl md:leading-[3.8rem]">
@@ -40,13 +41,13 @@ export default function HelloWorldPage() {
                 viewOptions={{ margin: '0px 0px 0px 0px' }}
                 transition={{ duration: 0.6, ease: 'easeIn', delay: 0.6 }}
               >
-              <div className="mt-6 mx-auto">
+              <p className="mt-6 mx-auto">
                       <Link href="/login">
                       <Button className="rounded-full outline outline-green-500/20 outline-7 outline-offset-4 hover:outline hover:outline-stone-300/30 hover:outline-2 hover:outline-offset-0  bg-gradient-to-r from-fuchsia-950 to-neutral-950 hover:bg-gradient-to-br hover:from-fuchsia-900 hover:to-neutral-900 py-6 px-8 font-normal text-sm text-stone-100 shadow-xl hover:shadow-lg transition-all duration-100 hover:scale-105 shadow-fuchsia-600/10">
                       Activate Elf Magic<SparkleIcon/>
                       </Button>
                       </Link>
-              </div>
+              </p>
               </InView>
           </div>
   
