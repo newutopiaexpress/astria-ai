45c9d1c7c6bb233f197b8a30733c2165d8eb1c55
utopiaexpress123 <basepaxton@gmail.com>
2024-12-10 00:46:54 +0100

    fgf

--- app/elf/page.tsx
+++ app/elf/page.tsx
@@ -88,9 +88,11 @@ export default function HelloWorldPage() {
       viewOptions={{ margin: '0px 0px 0px 0px' }}
       transition={{ duration: 0.6, ease: 'easeInOut', delay: 0.2 }}
   >
-    <div className="w-full grid grid-cols-12 pt-24 gap-0 mx-auto min-h-96 relative">
+    <div className="w-full grid grid-cols-12 pt-32 gap-0 mx-auto min-h-96 relative">
       <div className="col-span-12 px-6 text-center backdrop-blur-md">
-          <p className="mb-4 mx auto text-center border border-stone-300 rounded-full max-w-fit mx-auto text-sm px-4 py-1">No studio, no stress.</p>
+          <p className="mb-4 mx auto text-center border border-stone-300 rounded-full max-w-fit mx-auto text-sm px-4 py-1">
+            No studio, no stress.
+            </p>
           <h1 className="text-center text-stone-800 font-thin tracking-tight drop-shadow-sm [text-wrap:balance] text-4xl leading-[2.5rem] md:text-6xl md:leading-[3.8rem]">
           Picture This: <span className="bg-gradient-to-r from-stone-800  to-neutral-600 inline-block text-transparent bg-clip-text pb-2">You, But Even Better </span>
           </h1>
@@ -101,6 +103,13 @@ export default function HelloWorldPage() {
       <div className="py-12 col-span-12 max-w-full mx-auto bg-black/0">
         <ImageCarousel/>
         {/*<StyleSlider/>*/}
+      </div>
+      <div className="col-span-12 max-w-full mx-auto py-16">
+          <Link href="/login">
+              <Button className="rounded-full outline outline-rose-300/20 outline-7 outline-offset-4 hover:outline hover:outline-stone-300/30 hover:outline-2 hover:outline-offset-0  bg-gradient-to-l from-rose-950 to-neutral-950 hover:bg-gradient-to-br hover:from-rose-900 hover:to-neutral-900 py-6 px-8 font-normal text-sm text-stone-100 shadow-xl hover:shadow-lg transition-all duration-100 hover:scale-105 shadow-fuchsia-600/10">
+                Go to the Main Page<SparkleIcon/>
+              </Button>
+          </Link>
       </div>
     </div>
   </InView>
