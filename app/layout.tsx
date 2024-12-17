ea4181b21f4db2ba32e247fb99acf8834fa248f5
utopiaexpress123 <basepaxton@gmail.com>
2024-12-18 00:43:46 +0100

    fgff

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 children: React.ReactNode;
 }>) {
 return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <body className="flex flex-col bg-stone-200 h-screen overflow-x-hidden"> 
               <section className="">
               <ChristmasBanner/>
