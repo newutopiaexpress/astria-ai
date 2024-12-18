4a5de211257c3cc1942822aea6af1cf1bd0a267d
utopiaexpress123 <basepaxton@gmail.com>
2024-12-18 21:39:15 +0100

    fgf

--- components/realtime/ClientSideModelsList.tsx
+++ components/realtime/ClientSideModelsList.tsx
@@ -11,8 +11,6 @@ import ModelsTable from "../ModelsTable";
 import Image from "next/image";
 import { SparkleIcon } from "../SparkleIcon";
 import StripePricingTable from "@/components/stripe/StripeTable";
-import { Loader2 } from "lucide-react";
-import { PriceTable2 } from "../PriceTable2";
 
 export const revalidate = 0;
 
@@ -32,7 +30,6 @@ export default function ClientSideModelsList({
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
   );
   const [models, setModels] = useState<modelRowWithSamples[]>(serverModels);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const channel = supabase
@@ -66,30 +63,13 @@ export default function ClientSideModelsList({
   }, [supabase, models, setModels]);
 
   return (
-    <div id="train-model-container" className="mx-auto w-full z-30">
+    <div id="train-model-container" className="mx-auto max-w-[840px] w-full z-30">
       {models && models.length > 0 && (
-        <div className="mx-auto max-w-[840px] flex flex-col gap-4 rounded-2xl relative">
+        <div className="flex flex-col gap-4 rounded-2xl relative">
           <div className="flex flex-row gap-4 min-w-full mb-6 w-full items-center text-center">
-            <Link 
-              href="/overview/models/train" 
-              className="w-full mx-auto"
-              onClick={() => setIsLoading(true)}
-            >
-              <Button 
-                className="rounded-full bg-stone-800 text-stone-300 hover:bg-stone-700 hover:text-stone-200 px-6 py-6"
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Please wait
-                  </>
-                ) : (
-                  <>
-                    Create New Series
-                    <SparkleIcon/>
-                  </>
-                )}
+            <Link href="/overview/models/train" className="w-full mx-auto">
+              <Button className="rounded-full bg-stone-800 text-stone-300 hover:bg-stone-700 hover:text-stone-200 px-6 py-6">
+                Create New Series<SparkleIcon/>
               </Button>
             </Link>
           </div>
@@ -97,22 +77,21 @@ export default function ClientSideModelsList({
         </div>
       )}
       {models && models.length === 0 && (
-        <div className="max-w-[1200px] mx-auto animate-in h-full fade-in zoom-in flex flex-col gap-4 items-center justify-center relative">
-          <div className="z-30 mt-14">
+        <div className="animate-in h-full fade-in zoom-in flex flex-col gap-4 items-center justify-center relative">
+          <div className="z-30 mt-[27%]">
             <h1 className="text-2xl text-center mx-auto">
             {userName ? `Welcome ${userName}, Your story starts here!` : 'Your story starts here!'}
             </h1>
-            <div className="mx-auto text-center flex flex-row gap-4">
+            <div className="mx-auto text-center mt-4 flex flex-row gap-4">
               {userCredits === 0 ? (
-                <div>
-                <h1 className="pt-8 pb-4 text-center text-stone-800 font-bold tracking-tight drop-shadow-sm [text-wrap:balance] text-5xl leading-[3rem] md:text-6xl md:leading-[4.25rem]">
-                Studio Quality, <span className="font-thin bg-gradient-to-r from-stone-800 via-red-800  to-fuchsia-800 inline-block text-transparent bg-clip-text pb-2"> Minus the Studio Costs</span><br/>
-                </h1>
-                <PriceTable2/>
-                </div>
+                <Link href="/get-credits">
+                  <Button className="animate-jump-in animate-delay-300 p-5 transition-colors w-auto h-6 rounded-full text-center border-none bg-green-300 hover:bg-green-300/60 text-xs font-medium text-green-700" size={"lg"}>
+                    Buy Credits
+                  </Button>
+                </Link>
               ) : (
                 <Link href="/overview/models/train">
-                  <Button className="rounded-full text-xs border-stone-100 bg-stone-800 hover:bg-stone-600 text-stone-100" >
+                  <Button className="rounded-full text-xs border-stone-100 bg-stone-800 hover:bg-stone-600 text-stone-100" size={"lg"}>
                     Start Creating
                     <SparkleIcon/>
                   </Button>
