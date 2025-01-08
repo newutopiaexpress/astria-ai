22bb6c25159b7f8b6da95c0f7b5b369bb25a0d04
utopiaexpress123 <basepaxton@gmail.com>
2025-01-08 22:41:30 +0100

    ff

--- components/Navbar2.tsx
+++ components/Navbar2.tsx
@@ -63,7 +63,7 @@ export default async function Navbar() {
   const displayName = username === 'User' ? user?.email?.split('@')[0] : username;
 
   return (
-    <div className="fixed w-full top-0 h-24 md:h-20 bg-white/70 border border-px border-neutral-400/0 bg-backdrop-blur-sm backdrop-filter backdrop-blur-sm">
+    <div className="fixed w-full top-0 h-24 md:h-20 bg-white/70 border border-px border-neutral-400/0 bg-backdrop-blur-sm backdrop-filter backdrop-blur-sm shadow-2xl shadow-white">
       <div className="flex px-4 md:px-10 py-7 md:py-5 items-center justify-between z-50">
 
         {/* Logo section */}
